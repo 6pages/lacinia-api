369bcd1886aece8c3286fe0d6cdd1df092441823
Adam Tait <bin@adamta.it>
2019-09-20 14:25:17 -0700

    update user ns to inject server into dev-system

--- dev/user.clj
+++ dev/user.clj
@@ -3,6 +3,7 @@
             [clojure.data.json :as json]
             [clojure.tools.namespace.repl :refer (refresh refresh-all)]
             [com.stuartsierra.component :as component]
+            [com.sixpages.api-lacinia-pedestal-component.server :as server]
             [com.sixpages.api-lacinia-pedestal-component.system :as system]))
 
 
@@ -10,12 +11,20 @@
 
 (def user-system nil)
 
+(defn new-system []
+  (let [config (load-config)
+        server (server/new-component config)]
+    (-> config
+        system/new-system
+        (assoc
+         :server (component/using
+                   server
+                   [:service])))))
+
 (defn init []
   (alter-var-root
    #'user-system
-   (constantly
-    (system/new-system
-     (system/load-config)))))
+   (constantly (new-system))))
 
 (defn start []
   (alter-var-root
@@ -50,12 +59,12 @@
 ;; Basic tests
 
 (defn get-endpoint []
-  (let [config (system/load-config)
-        {:keys [protocol host port]} (:endpoint config)
-        s (str protocol "://" host)
-        ss (cond-> s
-             port (str ":" port))]
-    (str ss "/graphql")))
+  (let [config (config-by-env-k env-k)
+        {:keys [protocol host path port]} (:endpoint config)
+        s (str protocol "://" host)]
+    (cond-> s
+      port (str ":" port)
+      path (str "/" path))))
 
 
 ;; curl localhost:8888/graphql -X POST -H "Content-Type: application/graphql" -d "{ content(slug: \"/brief-template\") }"
