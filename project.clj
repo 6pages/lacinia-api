47c5dc96f1ba33cfc8fd2e4cd67ccbe8d320ced7
Adam Tait <bin@adamta.it>
2020-02-14 12:10:29 -0800

    major refactoring of project structure to separate pedestal & lambda based service builds

--- project.clj
+++ project.clj
@@ -1,58 +1,62 @@
 (defproject api-lacinia-pedestal-component "0.0.1-SNAPSHOT"
-  :description "Lacinia Pedestal Component API"
-  :url "http://github.com/adamtait/lacinia-pedestal-component"
+  :description "Lacinia-based GraphQL API service"
+  :url "http://github.com/adamtait/lacinia-pedestal-component-base"
 
   :min-lein-version "2.0.0"
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "0.4.500"]
                  
-                 ;; pedestal (web server)
-                 [io.pedestal/pedestal.service "0.5.7"
-                  :exclusions  [org.clojure/tools.analyzer.jvm
-                                org.clojure/core.async]]
-
                  ;; lacinia (graphql)
-                 #_[org.antlr/antlr4 "4.7.2"]
-                 #_[org.antlr/antlr4-runtime "4.7.2"]
-                 #_[clj-antlr "0.2.5"
-                    :exclusions [org.antlr/antlr4
-                                 org.antlr/antlr4-runtime]]
-                 #_[com.walmartlabs/lacinia "0.36.0"
-                  :exclusions [clj-antlr]]
-                 [com.walmartlabs/lacinia-pedestal "0.13.0"
-                  :exclusions [io.pedestal/pedestal.service
-                               ;;io.pedestal/pedestal.jetty
-                               ;;com.walmartlabs/lacinia
-                               ]]
+                 [com.walmartlabs/lacinia "0.36.0"]
 
                  ;; libraries
-                 [com.stuartsierra/component "0.4.0"]]
-  
-  :resource-paths ["config", "resources"]
+                 [com.stuartsierra/component "0.4.0"]
 
-  :profiles {:dev
-             {:source-paths ["dev" "dev-resources"]
-              :dependencies [
+                 ;; logging
+                 [ch.qos.logback/logback-classic "1.2.3"
+                  :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.26"]
+                 [org.slf4j/jcl-over-slf4j "1.7.26"]
+                 [org.slf4j/log4j-over-slf4j "1.7.26"]
+                 ]
+
+  :source-paths ["sources/base"]
+  :resource-paths ["config"
+                   "resources/schemas"]
+
+  :profiles {
+             :pedestal
+             {:dependencies [
+                             [io.pedestal/pedestal.service "0.5.7"
+                              :exclusions  [org.clojure/tools.analyzer.jvm
+                                            org.clojure/core.async]]
                              [io.pedestal/pedestal.jetty "0.5.7"
                               :exclusions [org.eclipse.jetty/jetty-util]]
                              [org.eclipse.jetty/jetty-util "9.4.20.v20190813"]
                              [io.pedestal/pedestal.service-tools "0.5.7"]
-                             [clj-http "3.10.0"]]}
-             :log
+                             [io.pedestal/pedestal.log "0.5.7"]
+                             
+                             [com.walmartlabs/lacinia-pedestal "0.13.0"
+                              :exclusions [io.pedestal/pedestal.service
+                                           io.pedestal/pedestal.jetty
+                                           com.walmartlabs/lacinia]]]
+              :source-paths ["sources/pedestal"]}
+             
+             :lambda
              {:dependencies [
-                             [ch.qos.logback/logback-classic "1.2.3"
-                              :exclusions [org.slf4j/slf4j-api]]
-                             [org.slf4j/jul-to-slf4j "1.7.26"]
-                             [org.slf4j/jcl-over-slf4j "1.7.26"]
-                             [org.slf4j/log4j-over-slf4j "1.7.26"]
-                             [io.pedestal/pedestal.log "0.5.7"]]}
-             
-             :lambda {:dependencies [
-                                     ;; running on AWS Lambda & accepting requests from API Gateway
-                                     [com.amazonaws/aws-lambda-java-core "1.2.0"]
-                                     [com.amazonaws/aws-lambda-java-events "2.2.6"]
-                                     [com.amazonaws/aws-lambda-java-log4j2 "1.0.0"]
-                                     ]}
-             
-             :uberjar {:aot [com.sixpages.api-lacinia-pedestal-component.handler]}})
+                             [com.amazonaws/aws-lambda-java-core "1.2.0"]
+                             [com.amazonaws/aws-lambda-java-events "2.2.6"]
+                             [com.amazonaws/aws-lambda-java-log4j2 "1.0.0"]]
+              :source-paths ["sources/lambda"]
+              :aot [com.sixpages.api-lacinia-pedestal-component.handler]}
+
+             :dev
+             {:dependencies [
+                             [org.clojure/tools.namespace "0.3.1"]
+                             [clj-http "3.10.0"]]
+              :source-paths ["sources/dev"]
+              :resource-paths ["resources/logging/dev"]}
+
+             :uberjar
+             {:resource-paths ["resources/logging/prod"]}})
