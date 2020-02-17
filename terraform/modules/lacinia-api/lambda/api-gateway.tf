771576f9496f7249a27d1e04c665abb65898b7c4
Adam Tait <bin@adamta.it>
2020-02-17 10:59:38 -0800

    add api-gateway support for both HTTP GET & POST

--- terraform/modules/lacinia-api/lambda/api-gateway.tf
+++ terraform/modules/lacinia-api/lambda/api-gateway.tf
@@ -23,17 +23,40 @@ resource "aws_api_gateway_resource" "proxy" {
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "proxy" {
+
+## GET request integration
+
+resource "aws_api_gateway_method" "get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_method.get.resource_id
+  http_method = aws_api_gateway_method.get.http_method
+
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.fn.invoke_arn
+}
+
+
+## POST request integration
+
+resource "aws_api_gateway_method" "post" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda" {
+resource "aws_api_gateway_integration" "post" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_method.proxy.resource_id
-  http_method = aws_api_gateway_method.proxy.http_method
+  resource_id = aws_api_gateway_method.post.resource_id
+  http_method = aws_api_gateway_method.post.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -58,7 +81,7 @@ resource "aws_lambda_permission" "apigw" {
 # Deployment
 
 resource "aws_api_gateway_deployment" "endpoint" {
-  depends_on = [aws_api_gateway_integration.lambda]
+  depends_on = [aws_api_gateway_integration.get, aws_api_gateway_integration.post]
 
   rest_api_id = aws_api_gateway_rest_api.api.id
 
